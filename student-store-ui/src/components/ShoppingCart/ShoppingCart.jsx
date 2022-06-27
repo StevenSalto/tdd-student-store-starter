a3063311eb02d9bdc842545aa82bc78c559a8556
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-27 02:21:41 -0400

    Update

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,19 +3,18 @@ import "./ShoppingCart.css"
 
 //props: isOpen, products, shoppingCart: itemId, quantity,
 export default function ShoppingCart({isOpen, products, shoppingCart}) {
+    var subtotal = 0;
     function CartTable(obj) {
         console.log(obj, products[obj.itemId])
         let product = products[obj.itemId - 1];
         let productsPrice = product.price * obj.quantity;
+        subtotal += productsPrice;
         return (
             <div className="product" key={obj.itemId}>
                 <div className="cart-product-name">{product.name}</div>
-                <div className="cart-product-quantity">{obj.quantity}</div>
-                <div className="cart-product-unit-price">{product.price}</div>
-                <div className="cart-product-price">{productsPrice}</div>
-                <div className="cart-subtotal"></div>
-                <div className="cart-taxes-and-fees"></div>
-                <div className="cart-total"></div>
+                <div className="cart-product-quantity">quantity: {obj.quantity}</div>
+                <div className="cart-product-unit-price">unit price: ${product.price}</div>
+                <div className="cart-product-price">quant. x unitprice: ${productsPrice}</div>
             </div>
         )
     }
@@ -23,6 +22,10 @@ export default function ShoppingCart({isOpen, products, shoppingCart}) {
     return (
         <div className="shopping-cart">
             {shoppingCart.map((e) => CartTable(e))}
+
+            <div className="cart-subtotal">subtotal: ${subtotal}</div>
+            <div className="cart-taxes-and-fees">taxes and fees: ${subtotal*0.0875}</div>
+            <div className="cart-total">total: ${subtotal*1.0875}</div>
         </div>
     )
 }
